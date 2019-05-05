edabdd371c39786bdb60ca1c409442d2f13e0452
Edison Chen <edison_cyh@hotmail.com>
2019-05-05 15:59:37 +0800

    minor changes

--- 001-exercise/03_cal_arithmetic sequence.py
+++ 001-exercise/03_cal_arithmetic sequence.py
@@ -1,4 +1,5 @@
 # 根据用户输入的开始数字与结束数字，计算等差数列的和
+print('It\'s a arithmetic sequence calculator.')
 x = int(input("input 1st number:"))
 y = int(input("input last number:"))
 sum = int((x + y) * (y - x + 1) / 2)
