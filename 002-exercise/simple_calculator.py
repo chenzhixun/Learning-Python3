37c7857d522ad5212527bf4699d3d52540e3b8cb
Edison Chen <edison_cyh@hotmail.com>
2019-05-07 21:24:50 +0800

    add simple calculator, using if statements

---
+++ 002-exercise/simple_calculator.py
@@ -0,0 +1,13 @@
+x = float(input("input 1st number:"))
+opr = input("input the operator:")
+y = float(input("input 2nd number:"))
+z = 0
+if opr == '*':
+    z = x * y
+elif opr == '/':
+    z = x / y
+elif opr == '-':
+    z = x - y
+elif opr == '+':
+    z = x + y   
+print("the result is:", z)
